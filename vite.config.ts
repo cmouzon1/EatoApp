18d46202931ee34d5cd33a7b5b354919b8488f32
cmouzon1 <cmouzonwfg@gmail.com>
2026-03-11 16:19:51 -0400

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -23,10 +23,11 @@ export default defineConfig({
     rollupOptions: {
       output: {
         manualChunks(id) {
-          if (id.includes("node_modules/react-dom/")) return "react-dom";
-          if (id.includes("node_modules/react/")) return "react";
-          if (id.includes("@clerk")) return "clerk";
-        },
+  if (id.includes("node_modules/react") || id.includes("node_modules/react-dom") || id.includes("node_modules/scheduler")) {
+    return "react-vendor";
+  }
+  if (id.includes("@clerk")) return "clerk";
+},
       },
     },
   },
